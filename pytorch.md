43f6490c5d3f83743742f4f5424d7a4c532d81dd
Vincent Garcia <vincentfpgarcia@gmail.com>
2019-08-21 17:20:26 +0200

    PyTorch: temporary code

--- pytorch.md
+++ pytorch.md
@@ -1,3 +1,3 @@
 # Pytorch
 
-## Datasets
+* [Datasets](pytorch/datasets.md)
--- pytorch/datasets.py
+++ pytorch/datasets.py
@@ -84,11 +84,117 @@ def example2():
 
     # Access the image and display it
     img_np = trainset.data[idx,:,:,:]
+    print(img_np.dtype)
     img_pil = Image.fromarray(img_np)
     img_pil.show()
+
+
+def example3():
+    
+    import numpy as np
+    import torch
+    import torchvision
+    import torchvision.transforms as transforms
+    from PIL import Image
+    import matplotlib.pyplot as plt
+
+    # Access the training set
+    datasets_dir = '~/datasets'
+    trainset = torchvision.datasets.CIFAR10(root=datasets_dir,
+                                            train=True,
+                                            download=True,
+                                            transform=transforms.ToTensor())
+
+    # Define the data loader
+    trainloader = torch.utils.data.DataLoader(trainset,
+                                              batch_size=4,
+                                              shuffle=True,
+                                              num_workers=2)
+
+    # Access the first image batch
+    dataiter = iter(trainloader)
+    images, labels = dataiter.next()
+
+    # def imshow(img):
+    #     img = img / 2 + 0.5     # unnormalize
+    #     npimg = img.numpy()
+    #     plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    #     plt.show()
+
+    # imshow(torchvision.utils.make_grid(images))
+
+    # print('make_grid', type(torchvision.utils.make_grid(images)))
+
+    # print(type(images))
+    # print(images.size())
+    # return
+
+    # Convert the torch.Tensor into a Numpy array
+    images_np = images.numpy()
+    print("Array size: %s" % str(images_np.shape))
+    print("Array type: %s" % images_np.dtype)
+
+    # tmp = np.transpose(images_np[0,:,:,:], (1, 2, 0))
+    # tmp = (tmp*255).astype(np.uint8)
+    # plt.imshow(tmp)
+    # plt.show()
+
+    tmp = np.zeros(256,256)
+    print(tmp.shape)
+
+
+    # f, ax = plt.subplots(1, 4)
+
+    # for i in range(4):
+    #     img = images_np[i,:,:,:]
+    #     img = np.transpose(img, (1, 2, 0))
+
+    #     ax[0].imshow(img)
+    # plt.show()
+
+    # tmp = np.transpose(images_np[0,:,:,:], (1, 2, 0))
+    # tmp = (tmp*255).astype(np.uint8)
+    # img_pil = Image.fromarray(tmp)
+    # img_pil.show()
+
+
+    # print(dir(images))
+    # print(type(images_np))
+    # print(images_np.shape)
+
+    # # Pick an image in the dataset
+    # idx = 20
+
+    # # Access the corresponding image label
+    # label = trainset.targets[idx]
+    # label_str = trainset.classes[label]
+    # print("Index = %d" % idx)
+    # print("Label = %d -> %s" % (label, label_str))
+
+    # Access the image and display it
+    # img_np = trainset.data[idx,:,:,:]
+    # print(type(img_np))
+    # img_pil = Image.fromarray(img_np)
+    # img_pil.show()
+
+
 
 
 if __name__ == '__main__':
     # example0()
     # example1()
-    example2()
+    # example2()
+    # example3()
+
+    # import numpy as np
+    import matplotlib
+    import matplotlib.pyplot as plt
+    # tmp = np.zeros((256,256,3), dtype=np.uint8)
+
+    # print(dir(matplotlib))
+
+    # print(tmp.shape)
+    # print(tmp.dtype)
+    plt.figure()
+    # plt.imshow(tmp)
+    plt.show()
